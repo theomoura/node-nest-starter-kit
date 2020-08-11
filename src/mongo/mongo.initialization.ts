2b7f2c54d1a57aff0a7feb3c9223b98ec00c202f
Theo Moura <theomouram@gmail.com>
2020-08-11 09:50:43 -0300

    Update mongo.initialization.ts

--- src/mongo/mongo.initialization.ts
+++ src/mongo/mongo.initialization.ts
@@ -2,6 +2,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 export function initMongo() {
   return MongooseModule.forRoot(
-    'mongodb+srv://admin:admin123@cluster0.y7bzx.mongodb.net/to-do?retryWrites=true&w=majority',
+    'mongodb+srv://admin:<password>@cluster0.y7bzx.mongodb.net/to-do?retryWrites=true&w=majority',
   );
 }
